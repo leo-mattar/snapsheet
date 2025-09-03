39adadbfcce82d9411b417070c2d90c66f9fc42d
Leonardo Mattar <leonardomattar.design@gmail.com>
2025-09-03 06:38:18 +0100

    Update resourcesHubSlider.js

--- src/components/resourcesHubSlider.js
+++ src/components/resourcesHubSlider.js
@@ -1,28 +1,35 @@
 export default function resourcesHubSlider() {
-  const sliderEl = document.querySelector(".swiper.re-hub");
-  if (!sliderEl) return;
+  const sliders = document.querySelectorAll(".swiper.re-hub");
+  if (!sliders.length) return;
 
-  const sliderMain = new Swiper(sliderEl, {
-    spaceBetween: 0,
-    speed: 600,
-    grabCursor: true,
-    navigation: {
-      prevEl: "#re-hub-prev",
-      nextEl: "#re-hub-next",
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1.15,
+  sliders.forEach((sliderEl, index) => {
+    const sliderMain = new Swiper(sliderEl, {
+      spaceBetween: 0,
+      speed: 600,
+      grabCursor: true,
+      navigation: {
+        // scope navigation to each slider
+        prevEl: sliderEl
+          .closest(".c-swiper.re-hub")
+          ?.querySelector("#re-hub-prev"),
+        nextEl: sliderEl
+          .closest(".c-swiper.re-hub")
+          ?.querySelector("#re-hub-next"),
       },
-      480: {
-        slidesPerView: 2.15,
+      breakpoints: {
+        320: {
+          slidesPerView: 1.15,
+        },
+        480: {
+          slidesPerView: 2.15,
+        },
+        768: {
+          slidesPerView: 2.8,
+        },
+        992: {
+          slidesPerView: 3,
+        },
       },
-      768: {
-        slidesPerView: 2.8,
-      },
-      992: {
-        slidesPerView: 3,
-      },
-    },
+    });
   });
 }
